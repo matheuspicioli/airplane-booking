d637b5e8e9f4b75bcfe8fd17aadb9bf7ef44a79c
Matheus Picioli <matheus.picioli@agencia.bid>
2021-02-06 22:28:07 -0300

    Finished the rules by donk way. Created a resource to normalize json response.

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -3,23 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Actions\CreateAirplaneStructure;
-use App\Models\Seat;
+use App\Http\Resources\SeatResource;
 use App\Repository\Contracts\BookingRepositoryContract;
 use App\SeatHelper;
+use App\Services\BookingService;
 use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
     private BookingRepositoryContract $booking_repository;
+    private BookingService $booking_service;
 
-    public function __construct(BookingRepositoryContract $booking_repository)
+    public function __construct(
+        BookingRepositoryContract $booking_repository,
+        BookingService $booking_service
+    )
     {
         $this->booking_repository = $booking_repository;
+        $this->booking_service = $booking_service;
     }
 
     public function store(Request $request)
     {
-//        dd(Seat::all());
+//        dd(SeatHelper::columnAlphabet(Aircraft::find(1)));
         $seats_to_reserve = $request->get('seats');
         $booking = $this->booking_repository->findOrFail(1);
         $seats_alread_booked = $booking->seats->map(fn($seat) => $seat->column.$seat->row);
@@ -33,11 +39,9 @@ class BookingController extends Controller
 
         $airplane_structure = (new CreateAirplaneStructure($request->all()))();
         $rows = $airplane_structure['rows'];
+        $seats = collect();
 
         /**
-         * if the $column == B, $next_column = A
-         * if the $column == E, $next_column = D
-         * @TODO: apply for all other rules
          * @TODO: analise all resets column to A, in same cases is need reset to D
          * @TODO: use the length and postion array to decide if is A or D /\
          */
@@ -49,19 +53,14 @@ class BookingController extends Controller
 
                 // 3 is the max seats per row arrangement
                 if ($seats_to_reserve === 1) {
-                    $seat = (new Seat)->fill(['row' => $row, 'column' => $column]);
-                    $seat->booking()->associate($booking);
-                    $seat->save();
-                    dd(SeatHelper::showAllSeats());
-                    break;
+                    $seats->add(SeatHelper::createSeat($booking, $row, $column));
+                    return SeatResource::collection($seats);
                 } else if ($seats_to_reserve === 2) {
                     if ($column === 'A' || $column === 'B' || $column === 'D' || $column === 'E') {
                         // Is not verified the nexts columns
                         for ($i = 0; $i < $seats_to_reserve; $i++) {
                             $next_column = chr(ord($column)+$i);
-                            $seat = (new Seat)->fill(['row' => $row, 'column' => $next_column]);
-                            $seat->booking()->associate($booking);
-                            $seat->save();
+                            $seats->add(SeatHelper::createSeat($booking, $row, $next_column));
                         }
                     } else if ($column === 'C' || $column === 'F') {
                         // Is not verified the nexts columns
@@ -71,22 +70,18 @@ class BookingController extends Controller
                             if ($i === 1) {
                                 $next_column = $column === 'C' ? 'A' : 'D';
                             }
-                            $seat = (new Seat)->fill(['row' => $next_row, 'column' => $next_column]);
-                            $seat->booking()->associate($booking);
-                            $seat->save();
+                            $seats->add(SeatHelper::createSeat($booking, $next_row, $next_column));
                         }
                     }
-                    dd(SeatHelper::showAllSeats());
-                    break;
+
+                    return SeatResource::collection(collect($seats));
                 } else if ($seats_to_reserve === 3) {
                     if ($column === 'A' || $column === 'D') {
                         // Booking in the same row, until 3 columns
                         // Is not verified the nexts columns
                         for ($i = 0; $i < $seats_to_reserve; $i++) {
                             $next_column = chr(ord($column)+$i);
-                            $seat = (new Seat)->fill(['row' => $row, 'column' => $next_column]);
-                            $seat->booking()->associate($booking);
-                            $seat->save();
+                            $seats->add(SeatHelper::createSeat($booking, $row, $next_column));
                         }
                     } else if ($column === 'B' || $column === 'E'){
                         // Is not verified the nexts columns
@@ -97,9 +92,7 @@ class BookingController extends Controller
                                 $next_row = $row+$i;
                                 $next_column = $column === 'B' ? 'A' : 'D';
                             }
-                            $seat = (new Seat)->fill(['row' => $next_row, 'column' => $next_column]);
-                            $seat->booking()->associate($booking);
-                            $seat->save();
+                            $seats->add(SeatHelper::createSeat($booking, $next_row, $next_column));
                         }
                     } else if ($column === 'C' || $column === 'F') {
                         // Is not verified the nexts columns
@@ -112,13 +105,11 @@ class BookingController extends Controller
                                 $next_column = $column === 'C' ? 'B' : 'E';
                                 $next_row = $next_row - 1;
                             }
-                            $seat = (new Seat)->fill(['row' => $next_row, 'column' => $next_column]);
-                            $seat->booking()->associate($booking);
-                            $seat->save();
+                            $seats->add(SeatHelper::createSeat($booking, $next_row, $next_column));
                         }
                     }
-                    dd(SeatHelper::showAllSeats());
-                    break;
+
+                    return SeatResource::collection($seats);
                 } else if ($seats_to_reserve === 4) {
                     if ($column === 'A' || $column === 'D') {
                         for ($i = 0; $i < $seats_to_reserve; $i++) {
@@ -129,9 +120,7 @@ class BookingController extends Controller
                                 $next_row = $next_row + 1;
                                 $next_column = $column;
                             }
-                            $seat = (new Seat)->fill(['row' => $next_row, 'column' => $next_column]);
-                            $seat->booking()->associate($booking);
-                            $seat->save();
+                            $seats->add(SeatHelper::createSeat($booking, $next_row, $next_column));
                         }
                     } else if ($column === 'B' || $column === 'E') {
                         for ($i = 0; $i < $seats_to_reserve; $i++) {
@@ -148,9 +137,7 @@ class BookingController extends Controller
                                 $next_row = $next_row + 1;
                                 $next_column = $column;
                             }
-                            $seat = (new Seat)->fill(['row' => $next_row, 'column' => $next_column]);
-                            $seat->booking()->associate($booking);
-                            $seat->save();
+                            $seats->add(SeatHelper::createSeat($booking, $next_row, $next_column));
                         }
                     } else if ($column === 'C' || $column === 'F') {
                         for ($i = 0; $i < $seats_to_reserve; $i++) {
@@ -167,13 +154,10 @@ class BookingController extends Controller
                                 $next_column = 'C';
                                 $next_row = $next_row + 1;
                             }
-                            $seat = (new Seat)->fill(['row' => $next_row, 'column' => $next_column]);
-                            $seat->booking()->associate($booking);
-                            $seat->save();
+                            $seats->add(SeatHelper::createSeat($booking, $next_row, $next_column));
                         }
                     }
-                    dd(SeatHelper::showAllSeats());
-                    break;
+                    return SeatResource::collection($seats);
                 } else if ($seats_to_reserve === 5) {
                     /**
                      * Identify the first or second side of aisle
@@ -183,11 +167,12 @@ class BookingController extends Controller
                             $next_column = chr(ord($column)+$i);
                             $next_row = $row;
 
-                            echo $next_column;
-                            if ($next_column === 'D') {
+                            if ($next_column === 'D' || $next_column === 'G') {
                                 $next_column = 'A';
                             } else if ($next_column === 'E') {
                                 $next_column = 'B';
+                            } else if ($next_column === 'F') {
+                                $next_column = 'C';
                             }
 
                             if ($column === 'A') {
@@ -205,20 +190,19 @@ class BookingController extends Controller
                                     $next_row = $next_row + 2;
                                 }
                             }
-                            $seat = (new Seat)->fill(['row' => $next_row, 'column' => $next_column]);
-                            $seat->booking()->associate($booking);
-                            $seat->save();
+                            $seats->add(SeatHelper::createSeat($booking, $next_row, $next_column));
                         }
                     } else if ($column === 'D' || $column === 'E' || $column === 'F') {
                         for ($i = 0; $i < $seats_to_reserve; $i++) {
                             $next_column = chr(ord($column)+$i);
                             $next_row = $row;
 
-                            echo $next_column;
-                            if ($next_column === 'G') {
+                            if ($next_column === 'G' || $next_column === 'J') {
                                 $next_column = 'D';
                             } else if ($next_column === 'H') {
                                 $next_column = 'E';
+                            } else if ($next_column === 'I') {
+                                $next_column = 'F';
                             }
 
                             if ($column === 'D') {
@@ -236,17 +220,153 @@ class BookingController extends Controller
                                     $next_row = $next_row + 2;
                                 }
                             }
-                            $seat = (new Seat)->fill(['row' => $next_row, 'column' => $next_column]);
-                            $seat->booking()->associate($booking);
-                            $seat->save();
+                            $seats->add(SeatHelper::createSeat($booking, $next_row, $next_column));
                         }
                     }
-                    dd(SeatHelper::showAllSeats());
-                    break;
+                    return SeatResource::collection($seats);
                 } else if ($seats_to_reserve === 6) {
+                    if ($column === 'A' || $column === 'B' || $column === 'C') {
+                        for ($i = 0; $i < $seats_to_reserve; $i++) {
+                            $next_column = chr(ord($column)+$i);
+                            $next_row = $row;
+
+                            if ($next_column === 'D' || $next_column === 'G') {
+                                $next_column = 'A';
+                            } else if ($next_column === 'E' || $next_column === 'H') {
+                                $next_column = 'B';
+                            } else if ($next_column === 'F' || $next_column === 'I') {
+                                $next_column = 'C';
+                            }
+
+                            if ($column === 'A') {
+                                if ($i === 3 || $i === 4 || $i === 5) {
+                                    $next_row = $row + 1;
+                                }
+                            } else if ($column === 'B') {
+                                if ($i === 2 || $i === 3 || $i === 4) {
+                                    $next_row = $row + 1;
+                                } else if ($i === 5) {
+                                    $next_row = $row + 2;
+                                }
+                            } else if ($column === 'C') {
+                                if ($i === 1 || $i === 2 || $i === 3) {
+                                    $next_row = $row + 1;
+                                } else if ($i === 4 || $i === 5) {
+                                    $next_row = $row + 2;
+                                }
+                            }
+
+                            $seats->add(SeatHelper::createSeat($booking, $next_row, $next_column));
+                        }
+                    } else if ($column === 'D' || $column === 'E' || $column === 'F') {
+                        for ($i = 0; $i < $seats_to_reserve; $i++) {
+                            $next_column = chr(ord($column)+$i);
+                            $next_row = $row;
+
+                            if ($next_column === 'G' || $next_column === 'J') {
+                                $next_column = 'D';
+                            } else if ($next_column === 'H' || $next_column === 'K') {
+                                $next_column = 'E';
+                            } else if ($next_column === 'I' || $next_column === 'L') {
+                                $next_column = 'F';
+                            }
+
+                            if ($column === 'D') {
+                                if ($i === 3 || $i === 4 || $i === 5) {
+                                    $next_row = $row + 1;
+                                }
+                            } else if ($column === 'E') {
+                                if ($i === 2 || $i === 3 || $i === 4) {
+                                    $next_row = $row + 1;
+                                } else if ($i === 5) {
+                                    $next_row = $row + 2;
+                                }
+                            } else if ($column === 'F') {
+                                if ($i === 1 || $i === 2 || $i === 3) {
+                                    $next_row = $row + 1;
+                                } else if ($i === 4 || $i === 5) {
+                                    $next_row = $row + 2;
+                                }
+                            }
+
+                            $seats->add(SeatHelper::createSeat($booking, $next_row, $next_column));
+                        }
+                    }
+                    return SeatResource::collection($seats);
                 } else if ($seats_to_reserve === 7) {
+                    if ($column === 'A' || $column === 'B' || $column === 'C') {
+                        for ($i = 0; $i < $seats_to_reserve; $i++) {
+                            $next_column = chr(ord($column)+$i);
+                            $next_row = $row;
+
+                            if ($next_column === 'D' || $next_column === 'G') {
+                                $next_column = 'A';
+                            } else if ($next_column === 'E' || $next_column === 'H') {
+                                $next_column = 'B';
+                            } else if ($next_column === 'F' || $next_column === 'I') {
+                                $next_column = 'C';
+                            }
+
+                            if ($column === 'A') {
+                                if ($i === 3 || $i === 4 || $i === 5) {
+                                    $next_row = $row + 1;
+                                } else if ($i === 6) {
+                                    $next_row = $row + 2;
+                                }
+                            } else if ($column === 'B') {
+                                if ($i === 2 || $i === 3 || $i === 4) {
+                                    $next_row = $row + 1;
+                                } else if ($i === 5 || $i === 6) {
+                                    $next_row = $row + 2;
+                                }
+                            } else if ($column === 'C') {
+                                if ($i === 1 || $i === 2 || $i === 3) {
+                                    $next_row = $row + 1;
+                                } else if ($i === 4 || $i === 5 || $i === 6) {
+                                    $next_row = $row + 2;
+                                }
+                            }
+
+                            $seats->add(SeatHelper::createSeat($booking, $next_row, $next_column));
+                        }
+                    } else if ($column === 'D' || $column === 'E' || $column === 'F') {
+                        for ($i = 0; $i < $seats_to_reserve; $i++) {
+                            $next_column = chr(ord($column)+$i);
+                            $next_row = $row;
+
+                            if ($next_column === 'G' || $next_column === 'J') {
+                                $next_column = 'D';
+                            } else if ($next_column === 'H' || $next_column === 'K') {
+                                $next_column = 'E';
+                            } else if ($next_column === 'I' || $next_column === 'L') {
+                                $next_column = 'F';
+                            }
+
+                            if ($column === 'D') {
+                                if ($i === 3 || $i === 4 || $i === 5) {
+                                    $next_row = $row + 1;
+                                } else if ($i === 6) {
+                                    $next_row = $row + 2;
+                                }
+                            } else if ($column === 'E') {
+                                if ($i === 2 || $i === 3 || $i === 4) {
+                                    $next_row = $row + 1;
+                                } else if ($i === 5 || $i === 6) {
+                                    $next_row = $row + 2;
+                                }
+                            } else if ($column === 'F') {
+                                if ($i === 1 || $i === 2 || $i === 3) {
+                                    $next_row = $row + 1;
+                                } else if ($i === 4 || $i === 5 || $i === 6) {
+                                    $next_row = $row + 2;
+                                }
+                            }
+
+                            $seats->add(SeatHelper::createSeat($booking, $next_row, $next_column));
+                        }
+                    }
+                    return SeatResource::collection($seats);
                 }
-                break;
             }
         }
     }
--- app/SeatHelper.php
+++ app/SeatHelper.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Models\Booking;
 use App\Models\Seat;
 
 class SeatHelper
@@ -9,5 +10,13 @@ class SeatHelper
     public static function showAllSeats(): array
     {
         return Seat::all()->map(fn($seat) => $seat->column.$seat->row)->toArray();
+    }
+
+    public static function createSeat(Booking $booking, int $row, string $column): Seat
+    {
+        $seat = (new Seat)->fill(['row' => $row, 'column' => $column]);
+        $seat->booking()->associate($booking);
+        $seat->save();
+        return $seat;
     }
 }
